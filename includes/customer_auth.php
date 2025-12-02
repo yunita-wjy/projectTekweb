39def10afaa16ec95943a164561da4bd2294d600
gizaangelica <gizaangelica28@gmail.com>
2025-12-02 19:49:10 +0700

    Changes php for admin_auth and customer_auth

--- includes/customer_auth.php
+++ includes/customer_auth.php
@@ -2,7 +2,7 @@
     session_start();
 
     if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'customer') {
-        header("Location: /cinema/auth/login.php");
+        header("Location: ../auth/login.php");
         exit();
     }
 
